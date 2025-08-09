0e2ef5d2938ae5405d0774b5f7c0dccfccaf4f8a
- Z e r o <kimianalconera39@gmail.com>
2025-08-09 15:56:34 +0800

    update: tamagui installed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { Stack } from 'expo-router'
+import { useColorScheme } from 'react-native'
+import { TamaguiProvider } from 'tamagui'
+
+import { tamaguiConfig } from '../tamagui.config'
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme()
+
+  return (
+    // add this
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
+  )
 }
